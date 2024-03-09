26e653d523a790e7936fc1e688ecb99c0f012745
miozune <miozune@gmail.com>
2024-03-09 10:48:04 +0900

    IJ cleanup: Simplifiable 'Map' operations

--- src/main/java/lumien/custommainmenu/configuration/Config.java
+++ src/main/java/lumien/custommainmenu/configuration/Config.java
@@ -10,11 +10,7 @@ public class Config {
 
     public void addGui(String name, GuiCustom gc) {
         int scale;
-        GuiEntry entry = this.guis.get(name);
-        if (entry == null) {
-            entry = new GuiEntry();
-            this.guis.put(name, entry);
-        }
+        GuiEntry entry = this.guis.computeIfAbsent(name, k -> new GuiEntry());
         if ((scale = gc.guiConfig.guiScale) == -1) {
             entry.standard = gc;
         } else if (scale == 0) {
