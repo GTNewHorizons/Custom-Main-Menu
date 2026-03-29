3d7d6f242815a205051f367de113d7217dfc4c78
Léa Gris <lea.gris@noiraude.net>
2026-03-29 13:16:53 +0200

    event rename

--- src/main/java/lumien/custommainmenu/events/ActionIMCEvent.java
+++ src/main/java/lumien/custommainmenu/events/ActionIMCEvent.java
@@ -6,12 +6,12 @@ import cpw.mods.fml.common.eventhandler.Event;
  * Fired on {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS} when a CMM button with action type
  * {@code "sendIMC"} is clicked.
  */
-public class SendIMCEvent extends Event {
+public class ActionIMCEvent extends Event {
 
     public final String modId;
     public final String message;
 
-    public SendIMCEvent(String modId, String message) {
+    public ActionIMCEvent(String modId, String message) {
         this.modId = modId;
         this.message = message;
     }
--- src/main/java/lumien/custommainmenu/lib/actions/ActionSendIMC.java
+++ src/main/java/lumien/custommainmenu/lib/actions/ActionSendIMC.java
@@ -2,12 +2,12 @@ package lumien.custommainmenu.lib.actions;
 
 import net.minecraftforge.common.MinecraftForge;
 
-import lumien.custommainmenu.events.SendIMCEvent;
+import lumien.custommainmenu.events.ActionIMCEvent;
 import lumien.custommainmenu.gui.GuiCustom;
 
 /**
- * CMM action that fires a {@link SendIMCEvent} on the Forge event bus when performed. Config: {@code "action": {"type":
- * "sendIMC", "modid": "targetmodid", "message": "someKey"}}
+ * CMM action that fires a {@link ActionIMCEvent} on the Forge event bus when performed. Config: {@code "action":
+ * {"type": "sendIMC", "modid": "targetmodid", "message": "someKey"}}
  */
 public class ActionSendIMC implements IAction {
 
@@ -21,6 +21,6 @@ public class ActionSendIMC implements IAction {
 
     @Override
     public void perform(Object source, GuiCustom menu) {
-        MinecraftForge.EVENT_BUS.post(new SendIMCEvent(targetModId, message));
+        MinecraftForge.EVENT_BUS.post(new ActionIMCEvent(targetModId, message));
     }
 }
